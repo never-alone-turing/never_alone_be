35cb9885a30a3d76ecb426a95fe1e4e9d5ed2fc2
Saryn Mooney <smooney15@my.whitworth.edu>
2020-12-19 14:37:12 -0700

    user spec with validations passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of :username}
+    it {should validate_uniqueness_of :username}
+    it {should validate_presence_of :email}
+    it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :avatar}
+    it {should validate_presence_of :phone}
+    it {should validate_uniqueness_of :phone}
+  end
 end
