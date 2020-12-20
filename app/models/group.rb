66d25e35d79d3521c48f81ddd078aa5ef468e55a
Nico Rithner <nicorithner@gmail.com>
2020-12-19 18:28:31 -0700

    Add relationships to user and group
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   validates :name, :description, presence: true
+
+  has_many :group_users
+  has_many :users, through: :group_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   validates :username, :email, :phone, uniqueness: true, presence: true
   validates :avatar, presence: true
+
+  has_many :group_users
+  has_many :groups, through: :group_users
+
   enum role: ['caretaker', 'lone_ranger']
 end
