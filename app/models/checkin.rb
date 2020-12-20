ffaa14cde23b4b1bbbf87b9eae4bd9ce429700d8
Daniel Lessenden <dless27@gmail.com>
2020-12-19 20:58:46 -0600

    Add relationships with user and group Co-authoerd-by: Saryn Mooney <smooney15@my.whitworth.edu> Co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,9 +1,9 @@
 class Checkin < ApplicationRecord
 
-  validates_presence_of :checkin_type, :time, :response
+  validates_presence_of :checkin_type, :time, :response, :user_id, :group_id
 
-  # belongs_to :user
-  # belongs_to :pack
+  belongs_to :user
+  belongs_to :group
 
   enum checkin_type: %w(wellness medication)
 
