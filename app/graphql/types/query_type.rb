fc781243f80190fbec87224043d5decbf945308a
Nico Rithner <nicorithner@gmail.com>
2020-12-30 11:02:40 -0700

    Add query find user by id
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,9 +5,18 @@ module Types
     null: false,
     description: "Returns a list of existing users"
 
+    field :user, Types::UserType, null: false do
+      description 'Find a user by ID'
+      argument :id, ID, required: true
+    end
+
     #---- Resolvers
     def all_users
       User.all
+    end
+
+    def user(id:)
+      User.find(id)
     end
   end
 end
