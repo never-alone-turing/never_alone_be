951f02afa5c2fbbc80f14768c76fc82deccf8c11
Nico Rithner <nicorithner@gmail.com>
2020-12-30 06:02:08 -0700

    Create all_users query type field and resolver
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,13 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :all_users,
+    [Types::UserType],
+    null: false,
+    description: "Returns a list of existing users"
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    #---- Resolvers
+    def all_users
+      User.all
     end
   end
 end
