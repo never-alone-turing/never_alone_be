5e549509e59983e55a56c6f13eee058484b36904
Nico Rithner <nicorithner@gmail.com>
2020-12-30 11:00:23 -0700

    Add user role column to schema
    Setted user as the default value
    
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_023041) do
+ActiveRecord::Schema.define(version: 2020_12_30_174745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_12_20_023041) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0
   end
 
   add_foreign_key "checkins", "groups"
