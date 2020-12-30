7515a8e17ed2318ac77b33a43c3daf39efaebe97
Nico Rithner <nicorithner@gmail.com>
2020-12-30 06:01:00 -0700

    Create GraphQL user_type.rb
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :username, String, null: false
+    field :email, String, null: false
+    field :avatar, String, null: true
+    field :phone, String, null: true
+  end
+end
