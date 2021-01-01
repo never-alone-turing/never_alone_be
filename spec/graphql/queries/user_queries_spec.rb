874ca082e3e32751c4d9db126c1b6e4528969cd6
Nico Rithner <nicorithner@gmail.com>
2021-01-01 10:50:59 -0700

    Create find_user_by_id query test
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- spec/graphql/queries/user_queries_spec.rb
+++ spec/graphql/queries/user_queries_spec.rb
@@ -3,32 +3,24 @@ require 'rails_helper'
 RSpec.describe Types::QueryType do
   describe 'users' do
     it 'Find a user by id' do
-      # user = FactoryBot.create(:user)
-      user = User.create(username: "Herb Gutmann1", email: "user-100@email.com", phone: "(346) 733-0084", role: 'user', avatar: "https://robohash.org/atquenisimolestias.png?size=300x300&set=set1")
+      user = User.create(username: "Herb Gutmann1", email: "user-1000@email.com", phone: "(346) 733-0084", role: 'user', avatar: "https://robohash.org/atquenisimolestias.png?size=300x300&set=set1")
+      user_id = user.id
 
       query_string = <<-GRAPHQL
         query {
-                findUserById(id: $id){
+                findUserById(id: "#{user_id}"){
+                  id
                   username
                 }
               }
       GRAPHQL
-      user_id = user.id
+
       result = NeverAloneBeSchema.execute(query_string, variables: { id: user_id })
-      ## When I run this and look in pry I get this:
-      # #<GraphQL::Query::Result @query=... @to_h={"errors"=>[{"message"=>"Variable $id is used by  but not declared", "locations"=>[{"line"=>2, "column"=>34}], "path"=>["query", "findUserById", "id"], "extensions"=>{"code"=>"variableNotDefined", "variableName"=>"id"}}]}>
-      # binding.pry
 
-      user_result = result["data"]["query"]
+      user_result = result["data"]["findUserById"]
 
-      expect(data).to include(
-        'id'              => be_present,
-        'username'           => 'Herb Gutmann1',
-        'email' => "user-1@email.com",
-        'phone' => "(346) 733-0084",
-        'role'           => 'user',
-        'avatar'          => "https://robohash.org/atquenisimolestias.png?size=300x300&set=set1"
-      )
+      expect(user_result['id']).to eq("#{user_id}")
+      expect(user_result['username']).to eq('Herb Gutmann1')
     end
   end
 end
