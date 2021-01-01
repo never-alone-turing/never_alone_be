f85849dd2564c3fb4aaefe46fb7a308dde8c438f
Nico Rithner <nicorithner@gmail.com>
2021-01-01 14:51:37 -0700

    Add mutation type update_user
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
-    field :add_user, mutation: Mutations::AddUser,
-      description: "Add a new user"
+    field :add_user, mutation: Mutations::AddUser, description: "Add a new user"
+    field :update_user, mutation: Mutations::UpdateUser, description: "Update an existing user"
   end
 end
