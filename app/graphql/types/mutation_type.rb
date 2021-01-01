17b96362a54fbb171e79ad7f608524a6014400c7
Nico Rithner <nicorithner@gmail.com>
2021-01-01 15:12:14 -0700

    Add destroy_user mutation type
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :add_user, mutation: Mutations::AddUser, description: "Add a new user"
     field :update_user, mutation: Mutations::UpdateUser, description: "Update an existing user"
+    field :destroy_user, mutation: Mutations::DestroyUser, description: "Destroy an existing user"
   end
 end
