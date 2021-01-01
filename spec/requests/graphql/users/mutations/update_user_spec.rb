97989b29e084b302fc87e0692dbc9919da1ec0b2
Nico Rithner <nicorithner@gmail.com>
2021-01-01 14:51:02 -0700

    Create update_user_spec.rb
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

---
+++ spec/requests/graphql/users/mutations/update_user_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Update User" do
+
+  it "Update an user" do
+    user = User.create(id: '100', username: "Daniel", email: "daniel@email.com", phone: "(346) 733-0084", role: 'user', avatar: "https://robohash.org/atquenisimolestias.png?size=300x300&set=set1")
+    
+    user_id = user.id
+
+    mutation_string = <<-GRAPHQL
+      mutation{
+        updateUser(
+          input:{
+            id: "100",
+            username: "Daniel Lessenden",
+            email: "daniel_lessenden@email",
+            avatar: "https://robohash.org/atquenisimolestias.png?size=300x300&set=set1"
+            phone: "(346) 733-0084", 
+            role: "user", 
+          } 	
+        )
+        {
+          user{
+            id
+            username
+            email
+          }
+        }
+      }
+    GRAPHQL
+
+    post graphql_path, params: {query: mutation_string}
+    result = JSON.parse(response.body)
+
+    expect(result).to eq({"data"=>{"updateUser"=>{"user"=>{"id"=>"100", "username"=>"Daniel Lessenden", "email"=>"daniel_lessenden@email"}}}})
+  end
+end
