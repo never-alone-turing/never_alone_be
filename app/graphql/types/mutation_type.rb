73f4da29cc12d15c348f2e8a0ee614b0e767b6f1
Nico Rithner <nicorithner@gmail.com>
2021-01-01 14:07:38 -0700

    Add mutation type add_user
    co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    co-authored-by: Daniel Lessenden <dless27@gmail.com>
    co-authored-by: Nico Rithner <nicorithner@gmail.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    field :add_user, mutation: Mutations::AddUser,
+      description: "Add a new user"
   end
 end
