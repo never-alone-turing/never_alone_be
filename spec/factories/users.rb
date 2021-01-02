6f7d50352ed7b69ddb19f7289dc799e5325c3164
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-02 12:44:26 -0700

    create factory bot for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Name.name }
+    username { Faker::Internet.unique.username }
     email { Faker::Internet.unique.email }
     phone { Faker::PhoneNumber.cell_phone }
     avatar { Faker::Avatar.image }
