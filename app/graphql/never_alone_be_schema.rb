c182e1999c04f72f5840633daee52103508b4066
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-02 12:43:20 -0700

    update graphql schema

--- app/graphql/never_alone_be_schema.rb
+++ app/graphql/never_alone_be_schema.rb
@@ -3,9 +3,9 @@ class NeverAloneBeSchema < GraphQL::Schema
   query(Types::QueryType)
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
-  use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
-
-  # Add built-in connections for pagination
-  use GraphQL::Pagination::Connections
+  # use GraphQL::Execution::Interpreter
+  # use GraphQL::Analysis::AST
+  #
+  # # Add built-in connections for pagination
+  # use GraphQL::Pagination::Connections
 end
