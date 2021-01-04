d99fa46c4a88ba065473b575adb5ccce43b9fd30
Daniel Lessenden <dless27@gmail.com>
2021-01-04 11:34:36 -0600

    Add window column to checkins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_174745) do
+ActiveRecord::Schema.define(version: 2021_01_04_172512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_174745) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.bigint "group_id"
+    t.datetime "window"
     t.index ["group_id"], name: "index_checkins_on_group_id"
     t.index ["user_id"], name: "index_checkins_on_user_id"
   end
