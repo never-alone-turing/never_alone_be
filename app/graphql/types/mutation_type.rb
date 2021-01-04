3421c185eeca46920f7344505ae64fb024da90f5
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-04 16:20:57 -0700

    add groups to mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,8 @@ module Types
     field :add_user, mutation: Mutations::AddUser, description: "Add a new user"
     field :update_user, mutation: Mutations::UpdateUser, description: "Update an existing user"
     field :destroy_user, mutation: Mutations::DestroyUser, description: "Destroy an existing user"
+    field :add_group, mutation: Mutations::AddGroup, description: "Add a new group"
+    field :update_group, mutation: Mutations::UpdateGroup, description: "Update an existing group"
+    #field :destroy_group, mutation: Mutations::DestroyGroup, description: "Destroy an existing group"
   end
 end
