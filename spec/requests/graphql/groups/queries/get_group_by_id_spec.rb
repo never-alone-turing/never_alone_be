bcf6e8077e1783a3a2e4fb224b81e9a0cde14cc5
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-04 13:42:51 -0700

    create groups folder inside spec

---
+++ spec/requests/graphql/groups/queries/get_group_by_id_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe 'groups' do
+    it 'can query a group by id' do
+      group1 = create(:group)
+      group2 = create(:group)
+
+      post graphql_path, params: { query: query(id: group1.id) }
+      result = JSON.parse(response.body)
+    
+      expect(result.count).to eq(1)
+      expect(result["data"]["findGroupById"]).to have_key("id")
+    end
+  end
+
+  def query(id:)
+    <<~GQL
+    query {
+      findGroupById(id: #{id}) {
+        id
+        name
+        description
+      }
+    }
+    GQL
+  end
+end
