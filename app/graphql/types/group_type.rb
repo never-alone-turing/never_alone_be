c931f1a8effb2a839e9e02c35a6ccd4e5b26e1dc
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-04 16:20:24 -0700

    update group type

--- app/graphql/types/group_type.rb
+++ app/graphql/types/group_type.rb
@@ -3,5 +3,8 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :description, String, null: false
+    field :group_users, [Types::GroupUserType], null: false
+    field :users, [Types::UserType], null: false
+    #field :checkins, [Types::CheckinType], null: false
   end
 end
