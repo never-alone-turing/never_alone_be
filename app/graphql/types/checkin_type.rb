d52ac76ab9926c0c12f6f356669604f5cf1f6e5a
Daniel Lessenden <dless27@gmail.com>
2021-01-05 15:01:34 -0600

    Add category to mutation file

--- app/graphql/types/checkin_type.rb
+++ app/graphql/types/checkin_type.rb
@@ -5,7 +5,7 @@ module Types
     field :response, Boolean, null: true
     field :window, GraphQL::Types::ISO8601DateTime, null: false
     field :user_id, ID, null: false
-    # field :group_id, ID, null: false
+    field :group_id, ID, null: false
 
     field :category, Enums::Category, null: false
 
