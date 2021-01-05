afb19319d7b904845682abb0e32b35fc4422893d
Daniel Lessenden <dless27@gmail.com>
2021-01-05 15:01:17 -0600

    Add category to mutation file

--- app/graphql/mutations/add_checkin.rb
+++ app/graphql/mutations/add_checkin.rb
@@ -6,18 +6,20 @@ module Mutations
     argument :user_id, String, required: false
     argument :group_id, String, required: false
     argument :window, String, required: false
+    argument :category, String, required: false
 
 
     field :checkin, Types::CheckinType, null: true
     field :errors, [String], null: false
 
-    def resolve(time: nil, response: nil, user_id: nil, group_id: nil, window: nil)
+    def resolve(time: nil, response: nil, user_id: nil, group_id: nil, window: nil, category: category)
       checkin = Checkin.create!(
         time: time,
         response: response,
         user_id: user_id,
         group_id: group_id,
-        window: window
+        window: window,
+        category: category
       )
 
       if checkin.save
