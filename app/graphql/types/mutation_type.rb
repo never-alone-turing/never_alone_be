0736f8d56f23c0a4055c38624acad4b78c575225
Daniel Lessenden <dless27@gmail.com>
2021-01-05 14:30:09 -0600

    Add the checkin type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,9 @@ module Types
     field :add_user, mutation: Mutations::AddUser, description: "Add a new user"
     field :update_user, mutation: Mutations::UpdateUser, description: "Update an existing user"
     field :destroy_user, mutation: Mutations::DestroyUser, description: "Destroy an existing user"
+
+    field :add_checkin, mutation: Mutations::AddCheckin, description: "Add a new checkin for a user"
+    field :update_checkin, mutation: Mutations::UpdateCheckin, description: "Update an existing checkin"
+    field :destroy_checkin, mutation: Mutations::DestroyCheckin, description: "Destroy an existing checkin"
   end
 end
