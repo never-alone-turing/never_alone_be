a6ec5baf7439e019bb2c23c6b8d8bc12e589e57f
Daniel Lessenden <dless27@gmail.com>
2021-01-05 14:25:17 -0600

    Create factory to seed DB with checkins

---
+++ spec/factories/checkin_factory.rb
@@ -0,0 +1,11 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :checkin do
+    checkin_type {rand(0..2)}
+    time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    window { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    user_id {rand(1..20)}
+    group_id {rand(1..20)}
+  end
+end
