35d6198663bd50434a731ecf8e71382c1e9b0b13
Daniel Lessenden <dless27@gmail.com>
2021-01-05 20:20:18 -0600

    Edit the data type for response, from boolean to integer(enum)

--- app/graphql/types/checkin_type.rb
+++ app/graphql/types/checkin_type.rb
@@ -2,7 +2,6 @@ module Types
   class CheckinType < Types::BaseObject
     field :id, ID, null: false
     field :time, GraphQL::Types::ISO8601DateTime, null: false
-    # field :response, Boolean, null: true
     field :window, GraphQL::Types::ISO8601DateTime, null: false
     field :user_id, ID, null: false
     field :group_id, ID, null: false
