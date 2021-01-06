8d2c3be8e0ba2d45540d664f3e7575bd1740912c
Daniel Lessenden <dless27@gmail.com>
2021-01-05 20:21:11 -0600

    Edit the data type for response, from boolean to integer(enum)

--- spec/requests/graphql/checkins/queries/checkin_spec.rb
+++ spec/requests/graphql/checkins/queries/checkin_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe "Checkin Query" do
     @user1 = User.create(username: "Herb Gutmann1", email: "user-1000@email.com", phone: "(346) 733-0084", role: 'user', avatar: "https://robohash.org/atquenisimolestias.png?size=300x300&set=set1")
     @user2 = User.create(username: "Other Gutmann1", email: "asd@email.com", phone: "(346) 733-0084", role: 'user', avatar: "https://robohash.org/atquenisimolestias.png?size=300x300&set=set1")
     @group = Group.create(name: "Group1", description: "it is a group")
-    @checkin1 = Checkin.create(category:0, time: DateTime.now, response: true, user_id: @user1.id, group_id: @group.id, window: DateTime.now)
-    @checkin2 = Checkin.create(category:1, time: DateTime.now, response: true, user_id: @user1.id, group_id: @group.id, window: DateTime.now)
-    @checkin3 = Checkin.create(category:0, time: DateTime.now, response: true, user_id: @user2.id, group_id: @group.id, window: DateTime.now)
+    @checkin1 = Checkin.create(category:0, time: DateTime.now, response: 0, user_id: @user1.id, group_id: @group.id, window: DateTime.now)
+    @checkin2 = Checkin.create(category:1, time: DateTime.now, response: 1, user_id: @user1.id, group_id: @group.id, window: DateTime.now)
+    @checkin3 = Checkin.create(category:0, time: DateTime.now, response: 2, user_id: @user2.id, group_id: @group.id, window: DateTime.now)
   end
 
   it "Find all checkins" do
@@ -28,7 +28,7 @@ RSpec.describe "Checkin Query" do
 
     post graphql_path, params: {query: query_string}
     result = JSON.parse(response.body)
-
+    # binding.pry 
     expect(result).to have_key("data")
     expect(result["data"]).to have_key("allCheckins")
     expect(result["data"]["allCheckins"].length).to eq(3)
