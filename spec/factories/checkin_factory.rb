eec461555ca746661927da32b7368956d80c5a4c
Daniel Lessenden <dless27@gmail.com>
2021-01-05 20:16:42 -0600

    Add seeds the old fashion way, having problems with Faker and foreign keys

--- spec/factories/checkin_factory.rb
+++ spec/factories/checkin_factory.rb
@@ -2,7 +2,8 @@ require 'faker'
 
 FactoryBot.define do
   factory :checkin do
-    checkin_type {rand(0..2)}
+    category {rand(0..2)}
+    response {rand(0..2)}
     time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     window { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     user_id {rand(1..20)}
