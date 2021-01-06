df4b8444d1f2c84d6326f31432d0bf62aeb421eb
Daniel Lessenden <dless27@gmail.com>
2021-01-05 18:04:38 -0600

    Change the data type for response

--- app/graphql/types/checkin_type.rb
+++ app/graphql/types/checkin_type.rb
@@ -2,12 +2,13 @@ module Types
   class CheckinType < Types::BaseObject
     field :id, ID, null: false
     field :time, GraphQL::Types::ISO8601DateTime, null: false
-    field :response, Boolean, null: true
+    # field :response, Boolean, null: true
     field :window, GraphQL::Types::ISO8601DateTime, null: false
     field :user_id, ID, null: false
     field :group_id, ID, null: false
 
     field :category, Enums::Category, null: false
+    field :response, Enums::Response, null: false
 
   end
 end
