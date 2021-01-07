486cb62fa61a1efad94fe5fc7bde465188df028c
Daniel Lessenden <dless27@gmail.com>
2021-01-06 19:13:32 -0600

    Reflect the changes of adding the name column to checkins

--- spec/requests/graphql/checkins/mutations/add_checkin_spec.rb
+++ spec/requests/graphql/checkins/mutations/add_checkin_spec.rb
@@ -18,7 +18,8 @@ RSpec.describe "Add Checkin" do
         time: "2021-01-04T21:12:19Z",
         window: "2021-01-04T21:12:19Z",
         userId: "#{@user1.id}",
-        groupId: "#{@group.id}"
+        groupId: "#{@group.id}",
+        name: "checkin1"
         }
         ){
           checkin{
@@ -28,6 +29,7 @@ RSpec.describe "Add Checkin" do
             window
             userId
             groupId
+            name
           }
         }
       }
@@ -39,6 +41,6 @@ RSpec.describe "Add Checkin" do
       expect(result).to have_key("data")
       expect(result["data"]).to have_key("addCheckin")
       expect(result["data"]["addCheckin"].length).to eq(1)
-      expect(result).to eq({"data"=>{"addCheckin"=>{"checkin"=>{"response"=>"Pending", "category"=>"Wellness", "time"=>"2021-01-04T21:12:19Z", "window"=>"2021-01-04T21:12:19Z", "userId"=>"#{@user1.id}", "groupId"=>"#{@group.id}"}}}})
+      expect(result).to eq({"data"=>{"addCheckin"=>{"checkin"=>{"response"=>"Pending", "category"=>"Wellness", "time"=>"2021-01-04T21:12:19Z", "window"=>"2021-01-04T21:12:19Z", "userId"=>"#{@user1.id}", "groupId"=>"#{@group.id}", "name"=>"checkin1"}}}})
   end
 end
