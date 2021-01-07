c2a2e3d2f3095810ad566e281e3d9a551c62bdcc
Daniel Lessenden <dless27@gmail.com>
2021-01-06 19:13:03 -0600

    Add name column to the checkin table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_235900) do
+ActiveRecord::Schema.define(version: 2021_01_07_005504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_01_05_235900) do
     t.bigint "group_id"
     t.datetime "window"
     t.integer "response", default: 0
+    t.string "name"
     t.index ["group_id"], name: "index_checkins_on_group_id"
     t.index ["user_id"], name: "index_checkins_on_user_id"
   end
