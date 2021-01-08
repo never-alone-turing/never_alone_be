ad9f2af8284ea06956cc2f171472d20a9ae1aa61
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-08 09:50:59 -0700

    sad path

--- spec/requests/graphql/groups/mutations/update_group_spec.rb
+++ spec/requests/graphql/groups/mutations/update_group_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Types::MutationType do
     it 'can update a group' do
       group = create(:group)
 
-      post graphql_path, params: { query: query(group.id) }
+      post graphql_path, params: { query: query1(group.id) }
       result = JSON.parse(response.body)
 
       expect(result["data"]).to have_key("updateGroup")
@@ -18,7 +18,7 @@ RSpec.describe Types::MutationType do
     end
   end
 
-  def query(id)
+  def query1(id)
     <<~GQL
     mutation {
       updateGroup(
@@ -38,4 +38,34 @@ RSpec.describe Types::MutationType do
     }
     GQL
   end
-end
+
+    it 'throws an error if a field is missing' do
+      group = create(:group)
+
+      post graphql_path, params: { query: query2(group.id) }
+      result = JSON.parse(response.body)
+
+      expect(result).to have_key("errors")
+      expect(result["errors"][0]).to have_key("message")
+      expect(result["errors"][0]["message"]).to eq( "Argument 'name' on InputObject 'UpdateGroupInput' is required. Expected type String!")
+    end
+
+  def query2(id)
+    <<~GQL
+    mutation {
+      updateGroup(
+        input: {
+          id: #{id},
+          description: "We're watching you. All the time"
+        }
+      )
+      {
+        group {
+          id
+          description
+        }
+      }
+    }
+    GQL
+  end
+  end
